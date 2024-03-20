a22641cf498e421e69cbe175463ce930359ed579
마도 <mydodo1120@naver.com>
2024-03-20 17:35:08 +0900

    1546153

--- Assets/Scripts/JSONManager.cs
+++ Assets/Scripts/JSONManager.cs
@@ -5,23 +5,61 @@ using System.IO;
 using UnityEngine.SceneManagement;
 using UnityEditor;
 
+
+[System.Serializable]
+public class PlayerData
+{
+    public float time;
+    public int score;
+}
+public class WriteData
+{
+    public PlayerData[] rank;
+}
+
 public class JSONManager : MonoBehaviour
 {
-    [System.Serializable]
-    public class PlayerData
-    {
-        public float time;
-        public int score;
-    }
-     
-    public static List<List<PlayerData>> _rankData;
+
+
+    public static List<List<PlayerData>> _rankData = new List<List<PlayerData>>();
 
     private string[] fileName = { "/desert.json", "/mountain.json", "/city.json" };
     string filePath;
 
+
+    //private void Awake()
+    //{
+    //    PlayerData eekta = new PlayerData();
+
+    //    eekta.time = 5;
+    //    eekta.score = 5;
+    //    _rankData.Add(new List<PlayerData>() { eekta });
+    //    SaveRank();
+    //}
     void Start()
     {
+        //DataSetting();
+
+        PlayerData _testPlayerData = new PlayerData();
+
+        _testPlayerData.time = 0.5f;
+        _testPlayerData.score = 5;
+
+        for (int i = 0; i < 3; i++)
+        {
+            _rankData.Add(new List<PlayerData>());
+            for (int j = 0; j < 5; j++)
+            {
+                _rankData[i].Add(_testPlayerData);
+            }
+        }
+
         DataSetting();
+    }
+
+    private void Update()
+    {
+        
     }
 
     void DataSetting()
@@ -32,16 +70,35 @@ public class JSONManager : MonoBehaviour
             if(File.Exists(filePath + fileName[i]))
             {
                 string json = File.ReadAllText(filePath+ fileName[i]);
-                _rankData[i] = JsonUtility.FromJson<List<PlayerData>>(json);
-            } else
-            {
+
+                WriteData rankDataList = JsonUtility.FromJson<WriteData>(json);
+                
                 for(int j = 0; j < 5; j++)
                 {
-                    _rankData.Add(new List<PlayerData>());
+                    _rankData[i][j] = rankDataList.rank[j];
                 }
-
-                File.WriteAllText(filePath + fileName[i], JsonUtility.ToJson(_rankData[i]));
+            } else
+            {
+                SaveRank();
+                break;
             }
+        }
+    }
+
+    void SaveRank()
+    {
+        filePath = Application.persistentDataPath;
+        string _tmpStr = "[";
+        for (int i = 0; i < 3; i++)
+        {
+            _tmpStr = "{ \"rank\": [";
+            for (int j = 0; j < 5; j++)
+            {
+                _tmpStr += JsonUtility.ToJson(_rankData[i][j]) + ((j < 4) ? "," : "]");
+            }
+            _tmpStr += "}";
+            File.Delete(filePath + fileName[i]);
+            File.WriteAllText(filePath + fileName[i], _tmpStr);
         }
     }
 }
